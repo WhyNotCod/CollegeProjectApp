60cff1a5cc0db8b643e80b093210e9bccfc17bc6
RenoVISION <poul2002.02@gmail.com>
2025-02-12 22:21:29 -0500

    adding scrolling to dev_reg

--- lib/findbleDev.dart
+++ lib/findbleDev.dart
@@ -6,44 +6,43 @@ class SelectBluetoothDevice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Select Bluetooth Device'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Searching for devices...'),
-
-            StreamBuilder<List<ScanResult>>(
-              //stream: FlutterBluePlus.isScanning,
-              stream: FlutterBluePlus.scanResults,
-              initialData: [],
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      ScanResult result = snapshot.data![index];
-                      return ListTile(
-                        title: Text(result.device.name),
-                        onTap: () {
-                          Navigator.pop(
-                              context, SelectedDevice(result.device, 0));
+        appBar: AppBar(
+          title: Text('Select Bluetooth Device'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Searching for devices...'),
+                StreamBuilder<List<ScanResult>>(
+                  //stream: FlutterBluePlus.isScanning,
+                  stream: FlutterBluePlus.scanResults,
+                  initialData: [],
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          ScanResult result = snapshot.data![index];
+                          return ListTile(
+                            title: Text(result.device.name),
+                            onTap: () {
+                              Navigator.pop(
+                                  context, SelectedDevice(result.device, 0));
+                            },
+                          );
                         },
                       );
-                    },
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
-    
+          ),
+        ));
   }
 }
